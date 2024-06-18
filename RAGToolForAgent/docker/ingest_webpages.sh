2c0aed6ec90e9f9cb7bf5aeeee955a8d5b3bc2d4
minmin-intel <minmin.hou@intel.com>
2024-06-18 16:47:22 -0700

    move data ingestion test to tests folder

--- RAGToolForAgent/docker/ingest_webpages.sh
+++
@@ -1,5 +0,0 @@
-link1="https://www.intc.com/news-events/press-releases/detail/1692/intel-reports-first-quarter-2024-financial-results"
-link2="https://www.intc.com/news-events/press-releases/detail/1672/intel-reports-fourth-quarter-and-full-year-2023-financial"
-curl -X POST "http://${host_ip}:6007/v1/dataprep" \
-     -H "Content-Type: multipart/form-data" \
-     -F 'link_list=[${link1},${link2}]' \
