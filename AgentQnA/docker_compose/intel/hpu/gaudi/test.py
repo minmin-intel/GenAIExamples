534b27eb1a901e7345ae5e488a333681d9ac7b4f
minmin-intel <minmin.hou@intel.com>
2024-09-27 15:07:09 -0700

    add test

---
+++ AgentQnA/docker_compose/intel/hpu/gaudi/test.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+
+
+def search_knowledge_base(query: str) -> str:
+    """Search the knowledge base for a specific query."""
+    # use worker agent (DocGrader) to search the knowledge base
+    url = os.environ.get("WORKER_AGENT_URL")
+    print(url)
+    proxies = {"http": ""}
+    payload = {
+        "query": query,
+    }
+    response = requests.post(url, json=payload, proxies=proxies)
+    return response.json()["text"]
+
+
+if __name__ == "__main__":
+    print(search_knowledge_base("Who is the best new artist in 2020?"))
