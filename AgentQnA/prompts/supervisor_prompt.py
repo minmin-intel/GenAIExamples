be4bfdedf9c81d21b8bb1eeb03d6983e4cbdb9fc
minmin-intel <minmin.hou@intel.com>
2025-02-24 17:59:25 +0000

    test prompt to get doc title

--- AgentQnA/prompts/supervisor_prompt.py
+++ AgentQnA/prompts/supervisor_prompt.py
@@ -30,7 +30,26 @@ Now take a deep breath and think step by step to answer user's question in this 
 USER MESSAGE: {input}
 """
 
+FINQA_PROMPT = """\
+You are a financial analyst. Given the question below, determine what financial document may contain the information needed to answer the question, for example, 10-K, 8-K, 10-Q, earnings report.
+Reason through the question and provide the name of the document in {{<COMPANY> <YEAR> <QUARTER (optional)> <DOC TYPE>}}.
+
+Examples:
+Question: What is the FY2018 capital expenditure amount (in USD millions) for Apple?
+Answer: Apple's FY2018 capital expenditure amount can be found in the 10-K filing for FY2018. So the document is {{Apple 2018 10-K}}.
+
+Question: Does Apple have a reasonably healthy liquidity in Q2 of 2023?
+Answer: Apple's liquidity profile in Q2 of 2023 can be found in the 10-Q filing for Q2 of 2023. So the document is {{Apple 2023 Q2 10-Q}}.
+
+Now, here is the question:
+{question}
+What financial document may contain the information needed to answer the question?
+"""
+
 from openai import OpenAI
+import pandas as pd
+import os
+import json
 
 def generate_answer(args, prompt):
     """
@@ -60,6 +79,13 @@ def generate_answer(args, prompt):
 
     return response
 
+def parse_response(response):
+    """
+    Parse the response from the LLM
+    """
+    return response.split("{")[-1].split("}")[0]
+    
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
@@ -67,34 +93,55 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, default="meta-llama/Llama-3.3-70B-Instruct")
     parser.add_argument("--max_new_tokens", type=int, default=4096)
     parser.add_argument("--temperature", type=float, default=0.5)
+    parser.add_argument("--test_supervisor", action="store_true")
     args = parser.parse_args()
 
-    rag_agent = """question_answering_agent - answer financial questions about a company. use this agent if want to get answers about financial details, args: ['query (string): should include company name and time, for example, which business unit had the highest growth for Microsoft in 2024.']"""
-    summary_agent = """summarization_agent - generate high-lelvel summary of a financial document, args: ['doc_title (string): the description of the document, should include company name and time, for example, Apple 2023 Q4 earnings call.']"""
-    research_agent = """research_agent - generate research report on a specified company with fundamentals analysis, sentiment analysis and risk analysis, args: ['company (string): the company name']"""
+    if args.test_supervisor:
+        rag_agent = """question_answering_agent - answer financial questions about a company. use this agent if want to get answers about financial details, args: ['query (string): should include company name and time, for example, which business unit had the highest growth for Microsoft in 2024.']"""
+        summary_agent = """summarization_agent - generate high-lelvel summary of a financial document, args: ['doc_title (string): the description of the document, should include company name and time, for example, Apple 2023 Q4 earnings call.']"""
+        research_agent = """research_agent - generate research report on a specified company with fundamentals analysis, sentiment analysis and risk analysis, args: ['company (string): the company name']"""
 
-    thread_history = ""
-    turn_history = ""
+        thread_history = ""
+        turn_history = ""
 
-    input_list = [
-        "What is the FY2018 capital expenditure amount (in USD millions) for 3M?",
-        "Does 3M have a reasonably healthy liquidity profile based on its quick ratio for Q2 of FY2023?",
-        "What drove operating margin change as of FY2022 for 3M?",
-        "Can you summarize Intel's Q1 2024 earnings call?",
-        "Key takeaways of the recent earnings call of Tesla?",
-        "Should I increase investment in Tesla?",
-    ]
+        input_list = [
+            "What is the FY2018 capital expenditure amount (in USD millions) for 3M?",
+            "Does 3M have a reasonably healthy liquidity profile based on its quick ratio for Q2 of FY2023?",
+            "What drove operating margin change as of FY2022 for 3M?",
+            "Can you summarize Intel's Q1 2024 earnings call?",
+            "Key takeaways of the recent earnings call of Tesla?",
+            "Should I increase investment in Tesla?",
+        ]
 
 
-    for input in input_list:
-        print(f"USER MESSAGE: {input}\n")
-        prompt = REACT_AGENT_LLAMA_PROMPT.format(
-            tools=f"{rag_agent}\n{summary_agent}\n{research_agent}",
-            thread_history=thread_history,
-            history=turn_history,
-            input=input
-        )
+        for input in input_list:
+            print(f"USER MESSAGE: {input}\n")
+            prompt = REACT_AGENT_LLAMA_PROMPT.format(
+                tools=f"{rag_agent}\n{summary_agent}\n{research_agent}",
+                thread_history=thread_history,
+                history=turn_history,
+                input=input
+            )
 
-        response = generate_answer(args, prompt)
-        print(response)
-        print("="*50)
+            response = generate_answer(args, prompt)
+            print(response)
+            print("="*50)
+        else:
+            WORKDIR=os.getenv('WORKDIR')
+            DATAPATH=os.path.join(WORKDIR, 'financebench/data/')
+            PDFPATH=os.path.join(WORKDIR, 'financebench/pdfs/')
+
+            filename = "financebench_open_source.jsonl"
+            df = pd.read_json(DATAPATH + filename, lines=True)
+
+            for i, row in df.iterrows():
+                query = row["question"]
+                doc_name = row["doc_name"]
+
+                print(f"Question: {query}")
+                prompt = FINQA_PROMPT.format(question=query)
+                response = generate_answer(args, prompt)
+                response = parse_response(response)
+                print(response)
+                print("="*50)
+                
